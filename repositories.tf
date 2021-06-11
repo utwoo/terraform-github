d923b89558a3b3e113d0c1d57c1e7a7858edd05e
Xiang Zhu <zhuxiang1212@hotmail.com>
2021-06-11 22:50:33 +0800

    Update repositories.tf

--- repositories.tf
+++ repositories.tf
@@ -1,27 +1,11 @@
 terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "utwoo-dev"
-#     storage_account_name = "utterraformbackend"
-#     container_name       = "general"
-#     key                  = "github-repo.tfstate"
-#   }
-
-#   required_providers {
-#     github = {
-#       source  = "integrations/github"
-#       version = "~> 4.0"
-#     }
-
+ required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 2.63.0"
     }
   }
 }
-
-# provider "github" {
-#   token = var.github_token
-# }
 
 provider "azurerm" {
   features {}
@@ -36,10 +20,3 @@ resource "azurerm_resource_group" "utwooGithubAction" {
   name     = "utwooGithubAction"
   location = "West US"
 }
-
-# module "terraform_test" {
-#   source = "./modules/repo-base"
-
-#   name        = "terraform-test"
-#   description = "this repo create by terraform."
-# }
