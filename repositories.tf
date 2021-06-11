7e358c59bfeccef9187960fac2ea2dc7640224bf
Xiang Zhu <zhuxiang1212@hotmail.com>
2021-06-11 19:45:18 +0800

    test

--- repositories.tf
+++ repositories.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "azurerm" {
+    storage_account_name = "tfstorage"
+    container_name       = "general"
+    key                  = "github-repo.tfstate"
+    use_msi              = true
+    subscription_id      = var.subscription_id
+    tenant_id            = var.tenant_id
+  }
+
   required_providers {
     github = {
       source  = "integrations/github"
@@ -11,9 +20,9 @@ provider "github" {
   token = var.github_token
 }
 
-module terraform_test {
-    source = "./modules/repo-base"
+module "terraform_test" {
+  source = "./modules/repo-base"
 
-    name = "terraform-test"
-    description = "this repo create by terraform."
+  name        = "terraform-test"
+  description = "this repo create by terraform."
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,8 @@
+# variables for github
 variable "github_token" {}
+
+# variables for azure
+variable "agent_client_id" {}
+variable "agent_client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
