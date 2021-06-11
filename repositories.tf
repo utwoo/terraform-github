485bf9b40df65994ea41c20f820cdc7ea00c6ef5
Xiang Zhu <zhuxiang1212@hotmail.com>
2021-06-11 20:13:53 +0800

    test

--- repositories.tf
+++ repositories.tf
@@ -25,6 +25,11 @@ provider "github" {
 
 provider "azurerm" {
   features {}
+
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.agent_client_id
+  client_secret   = var.agent_client_secret
 }
 
 module "terraform_test" {
