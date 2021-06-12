236861ad82e789abb792c0d4cf135029096cf604
Xiang Zhu <zhuxiang1212@hotmail.com>
2021-06-12 12:38:12 +0800

    test

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "repository_attributes" {
-  value = {
-    id            = module.terraform_test.repository_attributes.repo_id
-    full_name     = module.terraform_test.repository_attributes.full_name
-    html_url      = module.terraform_test.repository_attributes.html_url
-    git_clone_url = module.terraform_test.repository_attributes.git_clone_url
-  }
-}
